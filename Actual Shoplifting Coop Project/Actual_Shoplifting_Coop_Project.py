1510381eecf497f292ac8125cf2dc35f793b7d86
Sinéad Harrington <sineadharrington680@gmail.com>
2023-10-28 14:17:07 +0100

    added customer types

--- Actual Shoplifting Coop Project/Actual_Shoplifting_Coop_Project.py
+++ Actual Shoplifting Coop Project/Actual_Shoplifting_Coop_Project.py
@@ -1,9 +1,11 @@
-
+import random
 import time
+customer = random.randint(1,3)
 print("You are an employee at Co-op. You see a customer wandering around the store picking up items for a long time with a big backpack. ")
 time.sleep(1)
 print("you can: 1) Go up and confront them 2) Watch them 3) ask them if they need any help")
 time.sleep(1)
 choice = int(input("choose the number that corresponds with your choice: "))
-if choice == 1:
-    print("choice 1 example")
+if customer == 1:
+    if choice == 1:
+        print("The customer becomes violent.")
