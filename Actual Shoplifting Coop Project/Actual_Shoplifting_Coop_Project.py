a9a6436f506fe6d3edd5458b56aeb3246ce4cc0d
Sinéad Harrington <sineadharrington680@gmail.com>
2023-10-28 14:19:13 +0100

    added comments clarifying who each person is etc.

--- Actual Shoplifting Coop Project/Actual_Shoplifting_Coop_Project.py
+++ Actual Shoplifting Coop Project/Actual_Shoplifting_Coop_Project.py
@@ -6,6 +6,8 @@ time.sleep(1)
 print("you can: 1) Go up and confront them 2) Watch them 3) ask them if they need any help")
 time.sleep(1)
 choice = int(input("choose the number that corresponds with your choice: "))
+#customer types are as follows 1 is violent shoplifter 2 is normal person 3 is confused person
 if customer == 1:
     if choice == 1:
         print("The customer becomes violent.")
+# this is where you add next customer path with if statement 
